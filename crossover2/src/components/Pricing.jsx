8c422846fc6b5613f535f67f63767ff613215cec
Paul Lorber <paul.lorber@protonmail.com>
2024-03-25 15:53:58 +0100

    adjustments

--- crossover2/src/components/Pricing.jsx
+++ crossover2/src/components/Pricing.jsx
@@ -1,9 +1,37 @@
 import style from '../styles/Pricing.module.css';
+import imgBackground from '../public/bg/pricing.jpg';
+import imgMedaille from '../public/logo/pricing.gif';
 
 function Pricing () {
   return (
     <div className={style.container}>
-      <h1>Pricing</h1>
+      <img src={imgBackground} alt="background" className={style.background} />
+      <div className={style.headingContainer}>
+        <h1 className={style.heading}>Pricing</h1>
+      </div>
+      <div className={style.paragraphContainer}>
+        <p className={style.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.</p>
+      </div>
+    <div className={style.cardContainer}>
+      <div className={style.card}>
+        <img src={imgMedaille} alt="medaille" className={style.cardImage} />
+        <h2 className={style.cardHeading}>Basic</h2>
+        <p className={style.cardParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <button className={style.cardButton}>$150.00</button>
+      </div>
+      <div className={style.card}>
+      <img src={imgMedaille} alt="medaille" className={style.cardImage} />
+        <h2 className={style.cardHeading}>Pro</h2>
+        <p className={style.cardParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <button className={style.cardButton}>$200.00</button>
+      </div>
+      <div className={style.card}>
+      <img src={imgMedaille} alt="medaille" className={style.cardImage} />
+        <h2 className={style.cardHeading}>Premium</h2>
+        <p className={style.cardParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <button className={style.cardButton}>$250.00</button>
+      </div>
+    </div>
     </div>
   )
 }
