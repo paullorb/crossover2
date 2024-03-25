866077b7dede42e364afe25c9dc5cc6899f1891c
Paul Lorber <paul.lorber@protonmail.com>
2024-03-25 10:12:27 +0100

    main structure

---
+++ crossover2/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import style from '../styles/Footer.module.css';
+
+function Footer () {
+  return (
+    <div className={style.container}>
+      <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ crossover2/src/components/Pricing.jsx
@@ -0,0 +1,11 @@
+import style from '../styles/Pricing.module.css';
+
+function Pricing () {
+  return (
+    <div className={style.container}>
+      <h1>Pricing</h1>
+    </div>
+  )
+}
+
+export default Pricing;
