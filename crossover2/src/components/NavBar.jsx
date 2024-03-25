2805f56b5cbce6f46abcacc8338785fa98f95f8e
Paul Lorber <paul.lorber@protonmail.com>
2024-03-25 14:19:05 +0100

    navbar

--- crossover2/src/components/NavBar.jsx
+++ crossover2/src/components/NavBar.jsx
@@ -1,14 +1,22 @@
 import style from '../styles/NavBar.module.css';
+import Logo from '../public/logo/logo.gif';
+import imgSearch from '../public/icon/search.png';
 
 function NavBar () {
   return (
-    <nav className={style.navContainer}>
-      <h1>Logo</h1>
-      <div className={style.navLinks} >
-        <h1>Home</h1>
-        <h1>Product</h1>
-      </div>
-    </nav>
+    <div className={style.navContainer}>
+      <nav className={style.nav}>
+        <img className={style.logo} src={Logo} alt="logo" />
+        <div className={style.navLinks} >
+          <h1 className={style.navLink}>Home</h1>
+          <h1 className={style.navLink}>Product</h1>
+          <h1 className={style.navLink}>Promo</h1>
+          <h1 className={style.navLink}>About</h1>
+          <h1 className={style.navLink}>Contact</h1>
+        </div>
+        <img src={imgSearch} alt="search" className={style.search} />
+      </nav>
+    </div>
   )
 }
 
